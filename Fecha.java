bb7b5dbddfb3b6c86c023d3159bd7cfc6ab99521
Jesus A. Torres <101233168+Jesus-TA@users.noreply.github.com>
2024-02-12 18:31:13 -0500

    Add files via upload

---
+++ Fecha.java
@@ -0,0 +1,33 @@
+public class Fecha {
+
+	private int año;
+
+	private int mes;
+
+	private int dia;
+
+	public void definirAño(int año) {
+
+	}
+
+	public void definrMes(int mes) {
+
+	}
+
+	public void definirDia(int dia) {
+
+	}
+
+	public int obtenerAño() {
+		return 0;
+	}
+
+	public int obtenerMes() {
+		return 0;
+	}
+
+	public int obtenerDia() {
+		return 0;
+	}
+
+}
