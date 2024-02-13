50d9e2bbe75b36203b0f04f484073110f7444ae7
Jesus A. Torres <101233168+Jesus-TA@users.noreply.github.com>
2024-02-12 19:12:37 -0500

    Add files via upload

---
+++ Proyectil.java
@@ -0,0 +1,31 @@
+public class Proyectil {
+
+	private double velocidad;
+
+	private double angulo;
+
+	public void definirVelocidad(double velocidad) {
+
+	}
+
+	public void definirAngulo(double angulo) {
+
+	}
+
+	public double calcularAlcance() {
+		return 0;
+	}
+
+	public double calcularalturaMaxima() {
+		return 0;
+	}
+
+	public double mostrarVelocidad() {
+		return 0;
+	}
+
+	public double mostrarAngulo() {
+		return 0;
+	}
+
+}
